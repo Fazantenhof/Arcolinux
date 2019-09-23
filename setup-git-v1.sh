7d715f77194a0f5088fc05a7998422385f94589d
Fazantenhof <fazantenhofgithub@gmail.com>
2019-09-23 08:40:15 +0200

    Update

--- setup-git-v1.sh
+++ setup-git-v1.sh
@@ -69,6 +69,9 @@ git config --global credential.helper 'cache --timeout=25000'
 git config --global push.default simple
 
 
+#####
+
+
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
 echo "################################################################"
